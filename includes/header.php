5b5ed6c5020297d5cc351020a8dd39c7ad0cb732
Cristopher Jaramillo <cristopher.jaramillo98@gmail.com>
2025-11-19 19:39:48 -0500

    Ahora es responsive

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,10 @@
-<nav class="navbar navbar-expand-lg">
-    <div class="container">
+<nav class="navbar navbar-expand-lg navbar-dark">
+
+    <div class="container-fluid d-flex align-items-center">
 
         <a class="navbar-brand spa-link" data-page="inicio.php" href="#">Web Bio</a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -12,20 +13,17 @@
                 <li class="nav-item">
                     <a class="nav-link spa-link" data-page="inicio.php" href="#">Inicio</a>
                 </li>
-
                 <li class="nav-item">
                     <a class="nav-link spa-link" data-page="acerca.php" href="#">Acerca de</a>
                 </li>
-
                 <li class="nav-item">
                     <a class="nav-link spa-link" data-page="servicios.php" href="#">Servicios</a>
                 </li>
-
                 <li class="nav-item">
                     <a class="nav-link spa-link" data-page="contacto.php" href="#">Contacto</a>
                 </li>
             </ul>
         </div>
-        
+
     </div>
 </nav>
--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cristopher Bio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="./assets/css/styles.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body class="bg-light">
